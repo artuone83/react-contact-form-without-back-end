040d65fabe5f3a76002a6f89183d04258e328d9b
artuone83 <artur.wozniak@codetain.co>
2020-10-16 10:04:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple-react-contact-form-without-back-end
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/simple-react-contact-form-without-back-end)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+//https://medium.com/weekly-webtips/simple-react-contact-form-without-back-end-9fa06eff52d9
+
+// dependecies
+// npm install formik
+// npm install emailjs-com
+// npm install react-toastify
+
+// -- or --
+// paid option if more then 100 submissions
+// How to Integrate Netlify’s Form Handling in a React App
+// htps://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
+
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Contact Form without backend</h1>
+      <p>Contact me</p>
+    </div>
+  );
+}
